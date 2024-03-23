ebf87686a036d53f28c9a4b69d08e01bc911236d
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2024-03-23 15:03:31 -0400

    Ajout gestion connexion

--- CineQuebec.Windows/View/ConnexionControl.xaml.cs
+++ CineQuebec.Windows/View/ConnexionControl.xaml.cs
@@ -13,22 +13,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CineQuebec.Windows.DAL;
+using CineQuebec.Windows.DAL.Data;
 
 namespace CineQuebec.Windows.View
 {
-    /// <summary>
-    /// Logique d'interaction pour ConnexionControl.xaml
-    /// </summary>
-    public partial class ConnexionControl : UserControl
+	/// <summary>
+	/// Logique d'interaction pour ConnexionControl.xaml
+	/// </summary>
+    /// 
+	public partial class ConnexionControl : UserControl
     {
-        public ConnexionControl()
+
+		private DatabasePeleMele _db;
+
+		public ConnexionControl()
         {
             InitializeComponent();
+            _db = new DatabasePeleMele();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            ((MainWindow)Application.Current.MainWindow).AdminHomeControl();
+            string username = usernameTXT.Text.ToString();
+            string password = passwordTXT.Text.ToString();
+
+			if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+				MessageBox.Show("Veuillez entrer un nom d'utilisateur et un mot de passe.", "Erreur lors de la connexion", MessageBoxButton.OK, MessageBoxImage.Warning);
+            else
+			{
+				Administrateur admin = _db.ConnexionUtilisateur(username, password);
+
+                if (admin is not null)
+                {
+					((MainWindow)Application.Current.MainWindow).AdminHomeControl();
+				}
+			} 
         }
     }
 }
