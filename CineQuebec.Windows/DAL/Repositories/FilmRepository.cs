d242d2c0652783ba90ac9c7080534e538455df41
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2024-03-28 11:32:18 -0400

    +Film Repo

---
+++ CineQuebec.Windows/DAL/Repositories/FilmRepository.cs
@@ -0,0 +1,33 @@
+﻿using CineQuebec.Windows.DAL.Data;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineQuebec.Windows.DAL.Repositories
+{
+	public class FilmRepository:BaseRepository
+	{
+		public FilmRepository() { }
+
+		public List<Film> ChargerListeFilms()
+		{
+
+			List<Film> films = new List<Film>();
+
+			try
+			{
+
+				IMongoCollection<Film> collection = database.GetCollection<Film>(name: "Films");
+				films = collection.Aggregate().ToList();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Impossible d'obtenir la collection {ex.Message}", "Récupération Films");
+			}
+			return films;
+		}
+	}
+}
