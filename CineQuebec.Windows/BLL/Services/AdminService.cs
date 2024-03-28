0c8c43282c936fc3aac8aad0fca3c75bd9e33986
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2024-03-28 17:12:03 -0400

    Petits modification aux code Admin

--- CineQuebec.Windows/BLL/Services/AdminService.cs
+++ CineQuebec.Windows/BLL/Services/AdminService.cs
@@ -20,12 +20,11 @@ namespace CineQuebec.Windows.BLL.Services
             _adminRepository = new AdminRepository();
         }
 
-        public async Task<Administrateur> ConnexionUtilisateur(string pUsername, string pPassword)
+        public async Task<Administrateur?> ConnexionUtilisateur(string pUsername, string pPassword)
         {
-            Administrateur utilisateur = new Administrateur();
             try
             {
-                utilisateur = await _adminRepository.ConnexionUtilisateur(pUsername, pPassword);
+                return await _adminRepository.ConnexionUtilisateur(pUsername, pPassword);
 
             }catch (UtilisateurNotFoundException ex)
             {
@@ -35,7 +34,7 @@ namespace CineQuebec.Windows.BLL.Services
             {
 				MessageBox.Show($"Erreur lors de la connexion: {ex.Message}", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
 			}
-            return utilisateur;
+            return null;
         }
     }
 }
