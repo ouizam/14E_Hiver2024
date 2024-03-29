58f711dffcb032f5504332ee353a043be0123e8f
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2024-03-29 08:36:40 -0400

    Ajout page ajout d'un Film

---
+++ CineQuebec.Windows/View/AjouterFilm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CineQuebec.Windows.View
+{
+    /// <summary>
+    /// Logique d'interaction pour AjouterFilm.xaml
+    /// </summary>
+    public partial class AjouterFilm : Window
+    {
+        public AjouterFilm()
+        {
+            InitializeComponent();
+        }
+    }
+}
