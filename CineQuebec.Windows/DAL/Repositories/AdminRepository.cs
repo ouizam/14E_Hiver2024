ee5bc13382e20e7fc9460810f92a252aa7a54898
ouiza.meziani <elwizmeziani85@gmail.com>
2024-04-09 16:36:48 -0400

    Corriger la conncetion et le hachage de mot de passe

--- CineQuebec.Windows/DAL/Repositories/AdminRepository.cs
+++ CineQuebec.Windows/DAL/Repositories/AdminRepository.cs
@@ -9,6 +9,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Xml.Linq;
 
 namespace CineQuebec.Windows.DAL.Repositories
 {
@@ -21,12 +22,11 @@ namespace CineQuebec.Windows.DAL.Repositories
             _collection = database.GetCollection<Administrateur>("Administrateurs");
         }
 
-        public virtual async Task<Administrateur> ConnexionUtilisateur(string pUsername, byte[] pPassword)
+        public virtual async Task<Administrateur> ConnexionUtilisateur(string pUsername, string pPassword)
         {
             Administrateur utilisateur = new Administrateur();
             try
             {
-
                 var filter = Builders<Administrateur>.Filter.Eq("Name", pUsername);
 
                 utilisateur = await _collection.Find(filter).FirstOrDefaultAsync();
@@ -34,12 +34,14 @@ namespace CineQuebec.Windows.DAL.Repositories
                 if (utilisateur is null)
                     throw new UtilisateurNotFoundException("Le username est incorrect!");
 
-                if (!utilisateur.Password.Equals(pPassword)){
+                if (!utilisateur.Password.SequenceEqual(HacherMotDePasse(pPassword,  utilisateur.Salt))){
                     utilisateur = null;
                     throw new UtilisateurNotFoundException("Mot de passe incorrect!");
                 }
+            
 
-            }catch (UtilisateurNotFoundException  ex){
+            }
+            catch (UtilisateurNotFoundException  ex){
 				MessageBox.Show($"Erreur lors de la connexion: {ex.Message}", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
 			}
             catch (Exception ex)
@@ -49,6 +51,48 @@ namespace CineQuebec.Windows.DAL.Repositories
 
             return utilisateur;
         }
+
+        public byte[] HacherMotDePasse(string password, byte[] pSalt)
+        {
+            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password))
+            {
+                Salt = pSalt,
+                DegreeOfParallelism = 8,
+                Iterations = 4,
+                MemorySize = 1024 * 1024
+            };
+
+            return argon2.GetBytes(16);
+        }
+        public byte[] CreerSALT()
+        {
+            var buffer = new byte[16];
+            var rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(buffer);
+            return buffer;
+        }
+
+        //public void AddAdmin()
+        //{
+
+        //    var name = "admin";
+        //    var email = "admin@gmail.com";
+        //    var password = "Admin@12";
+        //    var salt = CreerSALT();
+        //    try
+        //    {
+        //        Administrateur admin = new Administrateur { Name = name, Email = email, Password = HacherMotDePasse(password, salt), Salt = salt };
+        //        var collection = database.GetCollection<Administrateur>("Administrateurs");
+        //        collection.InsertOne(admin);
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Impossible d'obtenir la collection " + ex.Message, "Erreur");
+        //    }
+
+        //}
+
 
     }
 }
